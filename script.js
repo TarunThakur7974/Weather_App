2e0ef75c9302f07e49acd6896755f66fae7923da
TarunThakur7974 <115872184+TarunThakur7974@users.noreply.github.com>
2023-07-05 16:53:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let dataFetch = async () => {
     }
 
     try {
-        let res = await fetch(`http://api.weatherapi.com/v1/current.json?key=9a3ef3e4ad19440889c104656230407&q=${placeName}&aqi=yes`)
+        let res = await fetch(`https://api.weatherapi.com/v1/current.json?key=650fe83132234e17a43112034230507&q=${placeName}&aqi=yes`)
         let data = await res.json();
         placeId.innerHTML = data.location.name
         tempretureId.textContent = `${data.current.temp_c}\xB0`
